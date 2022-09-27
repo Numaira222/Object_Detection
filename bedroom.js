6deebdc6af6b878691981532e85af358cfd923af
Numaira222 <77472414+Numaira222@users.noreply.github.com>
2022-09-26 18:57:17 -0700

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,34 @@
+img = "";
+status = "";
+ 
+function preload(){
+    img = loadImage("bedroom.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+    object_detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML= "Detecting Objects";
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420 );
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    object_detector.detect(img, gotResults);
+}
+
+function gotResults(error, results){
+if(error){
+    console.log(error);
+}
+else{
+    console.log(results);
+}
+}
+function back(){
+    window.location.replace("index.html");
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+function bedroom(){
+    window.location.replace("bedroom.html");
+}
+function bottle(){
+    window.location.replace("bottle.html");
+}
+function desk(){
+    window.location.replace("desk.html");
+}
+function basket(){
+    window.location.replace("fruit.html");
+}
+function room(){
+    window.location.replace("living_room.html");
+}
